15a238c2905eaa69fec1eb72fde08484c7dd1623
Kirk Dirk <jackalex@ya.ru>
2022-10-01 23:59:54 +0500

    add code

--- HWtask010.py
+++ HWtask010.py
@@ -1,7 +1,20 @@
 # Реализуйте алгоритм перемешивания списка
 
+import random
+
+
 sizelist = int(input('Введите размер списка: '))
 somelist = []
 for i in range(sizelist):
     somelist.append(i)
 print(somelist)
+stepofmix = 1000
+for i in range(stepofmix):
+    a = round(random.random()*(sizelist-1))
+    b = round(random.random()*(sizelist-1))
+    (somelist[a], somelist[b])=(somelist[b], somelist[a])
+print(somelist)
+
+# Простой способ
+# random.shuffle(somelist)
+# print(somelist)
